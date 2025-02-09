2ab83200c56aeabff28ea2570ccf936bb93b1e4b
Farid Ipkaev <2491287@mail.ru>
2025-02-09 21:11:43 +0300

    09022025211

--- file.py
+++ file.py
@@ -1,22 +1,15 @@
-line_count1 = sum(1 for line in open('1.txt')) #количество строк в файле
-line_count2 = sum(1 for line in open('2.txt'))
-line_count3 = sum(1 for line in open('3.txt'))
+def spisok_files(*file_names):
+    with open("new_file.txt", "a+") as new_file:  # Открываем файл для добавления
+        z = sorted(file_names, key=lambda x: sum(1 for line in open(x)), reverse=False)
+        for n in z:
+            new_file.write(n + '\n')
+            line_count = sum(1 for line in open(n))  # Подсчитываем количество строк
+            z = str(line_count)
+            new_file.write(z + '\n')
+            with open(n) as file:
+                for line in file:
+                    new_file.write(line)  # Добавляем содержимое файла
+                new_file.write('\n')
+    return
+spisok_files('1.txt', '2.txt', '3.txt')   
 
-if line_count1 > line_count2 and line_count1 > line_count3:
-    print(line_count1)
-    if line_count2 > line_count3:
-        print(line_count2)
-    else:
-        print(line_count3)
-elif line_count2 > line_count1 and line_count2 > line_count3:
-    print(line_count2)
-    if line_count1 > line_count3:
-        print(line_count1)
-    else:
-        print(line_count3)
-else:
-    print(line_count3)
-    if line_count1 > line_count2:
-        print(f'{line_count1} \n{line_count2}')
-    else:
-        print(line_count2,line_count1)
